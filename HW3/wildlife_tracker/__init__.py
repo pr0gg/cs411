a21933f4014c974dfd1b191936bc8b17f893531f
pr0gg <jeffreyl2040@gmail.com>
2024-10-06 18:33:48 -0400

    HW3

---
+++ HW3/wildlife_tracker/__init__.py
@@ -0,0 +1,7 @@
+from .animal_management import animal
+from .animal_management import animal_manager
+from .habitat_management import habitat
+from .habitat_management import habitat_manger
+from .migration_tracking import migration
+from .migration_tracking import migration_manager
+from .migration_tracking import migration_path
